ad83da5e53a7e7958020625e1beee89217a01306
Colton Peckham <coltonpeckham@gmail.com>
2023-01-03 11:44:29 -0600

    added favorites model

--- db/migrate/20230103174326_create_items.rb
+++ db/migrate/20230103174326_create_items.rb
@@ -3,7 +3,7 @@ class CreateItems < ActiveRecord::Migration[7.0]
     create_table :items do |t|
       t.string :name
       t.string :image_url
-      t.string :description
+      t.text :description
       t.string :category
 
       t.timestamps
